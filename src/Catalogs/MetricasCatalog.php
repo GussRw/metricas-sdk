52ffeb242352a28245a5661856c52ea07945fe68
gussrw <gussrw1@gmail.com>
2022-06-28 00:33:53 -0500

    feat: :sparkles: Catalog  class API was updated

--- src/Catalogs/MetricasCatalog.php
+++ src/Catalogs/MetricasCatalog.php
@@ -3,19 +3,19 @@
 namespace Plerk\Metricas\Catalogs;
 
 use Plerk\Metricas\ApiResource;
+use Plerk\Metricas\Authentication;
 use Plerk\Metricas\MetricasObject;
 
 class MetricasCatalog extends MetricasObject
 {
-    protected $uri;
-    protected $key;
+    protected static $uri;
+    protected static $key;
 
-    public array $items;
 
-    public function all(): array
+    public static function all(): array
     {
-        $response = ApiResource::get($this->uri, [], $this->authentication);
-        $this->items = $response[$this->key];
-        return $this->items;
+        $authentication = Authentication::login();
+        $response = ApiResource::get(static::$uri, [], $authentication);
+        return $response[static::$key];
     }
 }
